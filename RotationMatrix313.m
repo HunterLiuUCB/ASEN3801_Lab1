e45a5e9e5970528f590e67db60ce56f81fc1547a
Phimes45 <pahi6895@colorado.edu>
2026-01-28 13:59:34 -0700

    Refactor RotationMatrix313 for clarity and correctness

--- RotationMatrix313.m
+++ RotationMatrix313.m
@@ -1,19 +1,42 @@
 function DCM = RotationMatrix313(attitude313)
-% Inputs: 
+% Inputs:
+%   attitude313 – 3×1 vector of 3-1-3 Euler angles [alpha; beta; gamma],
+%                 corresponding to rotations about the z-, x-, and z-axes,
+%                 respectively
 %
-% Outputs: 
+% Outputs:
+%   DCM – Direction Cosine Matrix representing the transformation from the
+%         inertial North-East-Down (NED) frame to the body frame
 %
-% Methodology: 
+% Methodology:
+%   The Direction Cosine Matrix is constructed using a 3-1-3 Euler rotation
+%   sequence. Individual rotation matrices are defined for rotations about
+%   the z-axis, x-axis, and z-axis. These matrices are multiplied in reverse
+%   order of the Euler rotation sequence to form the overall passive
+%   transformation matrix from the inertial frame to the body frame. This
+%   formulation assumes a right-handed coordinate system and standard
+%   aerospace sign conventions.
+
 alpha = attitude313(1);
-beta = attitude313(2);
+beta  = attitude313(2);
 gamma = attitude313(3);
-% z-axis rotation
-r1 = [1 0 0; 0 cos(alpha) sin(alpha); 0 -sin(alpha) cos(alpha)];
-% x-axis rotation
-r2 = [cos(beta) 0 -sin(beta); 0 1 0; sin(beta) 0 cos(beta)];
-% z'-axis rotation
-r3 = [cos(gamma) sin(gamma) 0; -sin(gamma) cos(gamma) 0; 0 0 1];
+
+% z-axis rotation (first rotation)
+r3_alpha = [ cos(alpha)  sin(alpha) 0;
+            -sin(alpha)  cos(alpha) 0;
+             0           0          1];
+
+% x-axis rotation (second rotation)
+r1 = [1 0 0;
+      0 cos(beta)  sin(beta);
+      0 -sin(beta) cos(beta)];
+
+% z-axis rotation (third rotation)
+r3_gamma = [ cos(gamma)  sin(gamma) 0;
+            -sin(gamma)  cos(gamma) 0;
+             0           0          1];
 
 % Sequence 3-1-3
-DCM = r1 * r2* r3; % computes DCM in reverse order of the sequence
+DCM = r3_gamma * r1 * r3_alpha;  
+
 end
